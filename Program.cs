cfdde597b646fb8644cf268184307413ade861c0
dragonH <10220jack@gmail.com>
2022-09-14 19:00:28 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Amazon.Runtime.CredentialManagement;
+
+
+namespace UploadToS3Example {
+    class Program {
+        static async Task Main(string[] args) {
+            var chain = new CredentialProfileStoreChain();
+            // `profile_name` is aws profile name, need to be set up by aws cli first
+            if (chain.TryGetAWSCredentials("profile_name", out var credentials)) {
+                using (var s3Client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.APNortheast1)) {
+                    TransferUtility fileTransferUtility = new TransferUtility(s3Client);
+                    await fileTransferUtility.UploadAsync("./test.txt", "bucket_name", "test/test.txt");
+                };
+                
+            }
+            
+        }
+    }
+}
